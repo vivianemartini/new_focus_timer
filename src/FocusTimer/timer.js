c98a4d835d43b8858a99109c38f19849c021eabb
Viviane Martini <martini.viviane@gmail.com>
2023-08-28 21:30:18 -0300

    add timerjs

---
+++ src/FocusTimer/timer.js
@@ -0,0 +1,10 @@
+import state from './state.js'
+import * as el from './elements.js'
+
+export function updateDisplay(minutes,seconds){
+    minutes = minutes ?? state.minutes
+    seconds = seconds ?? state.seconds
+
+    el.minutes.
+}
+
